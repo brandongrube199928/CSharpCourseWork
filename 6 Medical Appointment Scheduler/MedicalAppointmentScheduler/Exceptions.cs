d423b2f93f27b28e8c1cf8c49b87b899882698de
Brandon Grube <brandongrube199928@gmail.com>
2026-03-08 20:20:02 -0500

    Medical Appointment Scheduler inital upload.

---
+++ 6 Medical Appointment Scheduler/MedicalAppointmentScheduler/Exceptions.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MedScheduler
+{
+    // Create a Business-rule exception for overlapping appointments (same provider or same room)
+    public class DoubleBookingException : Exception
+    {
+        public DoubleBookingException(string message) : base(message) { }
+    }
+
+    // Create a Business-rule exception for outside hours, short duration, etc.
+    public class InvalidAppointmentTimeException : Exception
+    {
+        public InvalidAppointmentTimeException(string message) : base(message){}
+    }
+
+}
